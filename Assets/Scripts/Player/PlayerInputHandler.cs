40084b305484822d69b43195f70b92c14e4b87f6
Benjamin Kershenabum <benji.kershenbaum@gmail.com>
2020-07-21 15:30:57 +0100

    Change to Input Manager. Multiplayer movement

---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour {
+    public int playerIndex=0;
+    private Vector2 inputDirection;
+    private PlayerMovement playerMovement;
+    private Controls controls = null;
+    private string a;
+    private void Awake() {
+        playerMovement = GetComponent<PlayerMovement>();
+        controls = new Controls();
+    }
+    private void OnEnable() {
+        controls.Player.Enable();
+    }
+    private void getInput() {
+        // movement
+        if (playerIndex == 0) {
+            inputDirection = controls.Player.P1Movement.ReadValue<Vector2>();
+            Debug.Log(controls.Player.P1Interact.ReadValue<double>());
+        }
+        else if (playerIndex == 1) {
+            inputDirection = controls.Player.P2Movement.ReadValue<Vector2>();
+        }
+
+    }
+
+
+
+    private void Update() {
+        getInput();
+        playerMovement.SetInputVector(inputDirection);
+    }
+
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     private float speed;
     private Vector2 movementDirection;
+    private Vector2 inputVector;
 
     private void Start() {
         movementDirection = Vector2.zero;
@@ -12,19 +13,22 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private void Update() {
-        getInput();
+        //getInput();
         move();
     }
 
-    private void getInput() {
-        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        speed = Input.GetKey(KeyCode.LeftShift) ? 2.0f : 1.0f;
-
-        return;
+    public void SetInputVector(Vector2 direction) {
+        inputVector = direction;
     }
 
+    //private void getInput() {
+    //    movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    //    speed = Input.GetKey(KeyCode.LeftShift) ? 2.0f : 1.0f;
+    //    return;
+    //}
+
     private void move() {
-        Vector3 translation = new Vector3(movementDirection.x, movementDirection.y, 0.0f) * speed * Time.deltaTime * TimeManager.getTimeFlow();
+        Vector3 translation = new Vector3(inputVector.x, inputVector.y, 0.0f) * speed * Time.deltaTime * TimeManager.getTimeFlow();
         
         transform.position += translation;
 
