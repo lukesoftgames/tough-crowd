5640f84e083c8456304a2c577bfc379eed194e69
freddiejbawden <freddiejbawden@gmail.com>
2020-07-23 11:06:45 +0100

    Add Blackboard

---
+++ Assets/Scripts/Peeps/Blackboard/Blackboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackboard : MonoBehaviour
+{
+    private Dictionary<string, object> blackboardValues;
+
+    public Blackboard()
+    {
+        blackboardValues = new Dictionary<string, object>();
+    }
+
+    public void UpdateBlackboard(string key, object value)
+    {
+        if (blackboardValues.ContainsKey(key))
+        {
+            blackboardValues[key] = value;
+        } else
+        {
+            blackboardValues.Add(key, value);
+        }
+    }
+    public void Clear()
+    {
+        foreach(string key in blackboardValues.Keys)
+        {
+            blackboardValues.Remove(key);
+        }
+    }
+   
+}
--- Assets/Scripts/Peeps/PeepAI.cs
+++ Assets/Scripts/Peeps/PeepAI.cs
@@ -9,6 +9,7 @@ public class PeepAI : MonoBehaviour
     public BehaviourTree behaviourTree;
     private BehaviourNode behaviourTreeRoot;
     private Context context = Context.GetInstance();
+    private Blackboard blackboard;
 
 
     void Start()
@@ -17,6 +18,7 @@ public class PeepAI : MonoBehaviour
         XNode.Node entry = ((BehaviourTree)behaviourTree.Copy()).FindEntryNode();
         behaviourTreeRoot = (BehaviourNode)entry.GetOutputPort("child").GetConnection(0).node;
         behaviourTreeRoot.Reset();
+        blackboard = new Blackboard();
     }
 
     void FixedUpdate()
